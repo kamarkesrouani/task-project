5304b5b9cdeb31e4b22431c74cbae72ef3982c96
jeanraphaelricha <40365578+jeanraphaelricha@users.noreply.github.com>
2018-06-18 21:19:42 +0300

    Create workshop.c

---
+++ workshop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  printf("Git project");
+  printf("Git project1");
+  printf("Git project2");
+  printf("Git project3");
+  printf("Git project4");
+  return 0;
+}
